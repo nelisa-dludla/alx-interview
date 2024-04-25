af997305d46271d8daca3c6c29e5a98b443bca62
Nelisa <nelisa.dludla@outlook.com>
2024-04-26 00:55:48 +0200

    Add solution to task 0 along with README

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,43 @@
+
+'''
+This function returns a list of lists of integers representing the Pascal’s triangle of n
+'''
+
+def pascal_triangle(n):
+    '''Computes the pascal triangle'''
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    # Iterate for the number of n rows
+    for loop in range(n):
+        if loop == 0:
+            new_row = calc_new_row(triangle)
+        else:
+            new_row = calc_new_row(triangle[loop - 1])
+
+        triangle.append(new_row)
+
+    return triangle
+
+def calc_new_row(prev_row):
+    '''Computes the values for a new row'''
+    if not prev_row:
+        return [1]
+
+    prev_row_copy = prev_row[:]
+
+    new_row = []
+    prev_row_copy.insert(0, 0)
+    prev_row_copy.insert(len(prev_row_copy), 0)
+    i = 0
+
+    for i in range(len(prev_row_copy)):
+        if i + 2 > len(prev_row_copy):
+            break
+
+        result = prev_row_copy[i] + prev_row_copy[i + 1]
+        new_row.append(result)
+
+    return new_row
