5cdafd7916720dabe1096a3a3d67a1061f1aae5a
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-06-13 01:19:33 +0200

    Added solution to interview question

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''Contains rotate_2d_matrix(matrix)
+'''
+
+
+def rotate_2d_matrix(matrix):
+    '''Rotates a 2D matrix 90 degrees
+    '''
+    left = 0
+    right = len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top = left
+            bottom = right
+
+            # Top Left -> Temp
+            temp = matrix[top][left + i]
+            # Bottom Left -> Top Left
+            matrix[top][left + i] = matrix[bottom - i][left]
+            # Bottom Right -> Bottom Left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            # Top Right -> Bottom Right
+            matrix[bottom][right - i] = matrix[top + i][right]
+            # Temp -> Top Right
+            matrix[top + i][right] = temp
+        # Move to inner space
+        left += 1
+        right -= 1
+
