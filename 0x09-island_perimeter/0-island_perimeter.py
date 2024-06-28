32ef45ec539fd1d4388a186521a41a8e2b13416c
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-06-28 04:23:47 +0200

    Updated 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,35 +2,28 @@
 ''' Soluton to interview question Island Perimeter
 '''
 
+
 def island_perimeter(grid):
     '''Returns the perimeter of the "grid"
     '''
-    # Store visited cells
-    visited = set()
-    grid_length = len(grid)
+    # Check if grid is valid
+    if not grid:
+        return 0
 
-    def explore_island(row, col):
-        '''Checks if cell is part of the island or sea
-        '''
-        if row >= grid_length or col >= grid_length or row < 0 or col < 0 or grid[row][col] == 0:
-                return 1
-        if (row, col) in visited:
-            return 0
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
 
-        visited.add((row, col))
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                # Start with all 4 sides
+                perimeter += 4
+                # Check cell above for land
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                # Check cell on the left for land
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
 
-        # Right
-        perimeter = explore_island(row, col + 1)
-        # Down
-        perimeter += explore_island(row + 1, col)
-        # Left
-        perimeter += explore_island(row, col - 1)
-        # Up
-        perimeter += explore_island(row - 1, col)
-
-        return perimeter
-
-    for row in range(grid_length):
-        for col in range(grid_length):
-            if grid[row][col]:
-                return explore_island(row, col)
+    return perimeter
