a5bca54c878a543bcfe518b9aaedb5e0b2a142c4
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-06-28 04:05:58 +0200

    Added solution to the island perimeter question, along with README

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+''' Soluton to interview question Island Perimeter
+'''
+
+def island_perimeter(grid):
+    '''Returns the perimeter of the "grid"
+    '''
+    # Store visited cells
+    visited = set()
+    grid_length = len(grid)
+
+    def explore_island(row, col):
+        if row >= grid_length or col >= grid_length or row < 0 or col < 0 or grid[row][col] == 0:
+                return 1
+        if (row, col) in visited:
+            return 0
+
+        visited.add((row, col))
+
+        # Right
+        perimeter = explore_island(row, col + 1)
+        # Down
+        perimeter += explore_island(row + 1, col)
+        # Left
+        perimeter += explore_island(row, col - 1)
+        # Up
+        perimeter += explore_island(row - 1, col)
+
+        return perimeter
+
+    for row in range(grid_length):
+        for col in range(grid_length):
+            if grid[row][col]:
+                return explore_island(row, col)
